ecf541e9c7059478165bbdc5057feda669bb2b8f
Phil. Austermann <phil@patient-radio.de>
2013-04-22 21:30:54 +0200

    # refactor tests & descriptions

--- test/unit/mJs.convert.Speed.js
+++ test/unit/mJs.convert.Speed.js
@@ -7,35 +7,36 @@ describe("measurement.Unit.Speed", function() {
 		expect(typeof speedUnit).toBe('object');
 	});
 
-	it('which constant string keys', function() {
+	it('with constant string keys', function() {
 		for (var i in speedUnit) {
 			expect(typeof speedUnit[i]).toBe('string');
 			expect(speedUnit[i].length).toBeGreaterThan(0);
 		}
 	});
 
-	describe("", function() {
-		it("returns 36 for mJs('Speed').convert(10).from(speedUnit.METRE_PER_SECOND).to(speedUnit.KILOMETRE_PER_HOUR)", function() {
+	var convertSpeed = measurement('Speed').convert;
+	describe("var convertSpeed = measurementJs('Speed').convert;", function() {
+		it("returns 36 for convertSpeed(10).from(speedUnit.METRE_PER_SECOND).to(speedUnit.KILOMETRE_PER_HOUR)", function() {
 			expect(
-				measurement('Speed').convert(10).from(speedUnit.METRE_PER_SECOND).to(speedUnit.KILOMETRE_PER_HOUR)
+				convertSpeed(10).from(speedUnit.METRE_PER_SECOND).to(speedUnit.KILOMETRE_PER_HOUR)
 				).toBe(36);
 		});
 
-		it("returns 10 for mJs('Speed').convert(36).to(speedUnit.METRE_PER_SECOND).from(speedUnit.KILOMETRE_PER_HOUR)", function() {
+		it("returns 10 for convertSpeed(36).to(speedUnit.METRE_PER_SECOND).from(speedUnit.KILOMETRE_PER_HOUR)", function() {
 			expect(
-				measurement('Speed').convert(36).to(speedUnit.METRE_PER_SECOND).from(speedUnit.KILOMETRE_PER_HOUR)
+				convertSpeed(36).to(speedUnit.METRE_PER_SECOND).from(speedUnit.KILOMETRE_PER_HOUR)
 				).toBe(10);
 		});
 
-		it("returns 80.5297065 for mJs('Speed').convert(36).from(speedUnit.METRE_PER_SECOND).to(speedUnit.MILES_PER_HOUR)", function() {
+		it("returns 80.5297065 for ...(36).from(speedUnit.METRE_PER_SECOND).to(speedUnit.MILES_PER_HOUR)", function() {
 			expect(
-				parseFloat((measurement('Speed').convert(36).from(speedUnit.METRE_PER_SECOND).to(speedUnit.MILES_PER_HOUR)).toFixed(8))
+				parseFloat((convertSpeed(36).from(speedUnit.METRE_PER_SECOND).to(speedUnit.MILES_PER_HOUR)).toFixed(8))
 				).toBe(80.52970651);
 		});
 
 		it("returns 6.21371192 for mJs('Speed').convert(10).from(speedUnit.KILOMETRE_PER_HOUR).to(speedUnit.MILES_PER_HOUR)", function() {
 			expect(
-				parseFloat((measurement('Speed').convert(10).from(speedUnit.KILOMETRE_PER_HOUR).to(speedUnit.MILES_PER_HOUR)).toFixed(8))
+				parseFloat((convertSpeed(10).from(speedUnit.KILOMETRE_PER_HOUR).to(speedUnit.MILES_PER_HOUR)).toFixed(8))
 				).toBe(6.21371192);
 		});
 
