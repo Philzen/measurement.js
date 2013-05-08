7110d9eec9cf4fd2d7c5f9bd4a6dc4c0b29743e4
Phil. Austermann <phil@patient-radio.de>
2013-05-08 03:08:38 +0200

    # removed console.log

--- test/unit/mJs.convert.Pressure.js
+++ test/unit/mJs.convert.Pressure.js
@@ -2,7 +2,7 @@
 
 describe("measurement.Unit.Pressure", function() {
 	expect(measurement.Unit.Pressure).toBeDefined();
-	
+
 	var pc = measurement.Unit.Pressure;
 	it('is an object', function() {
 		expect(typeof pc).toBe('object');
@@ -14,20 +14,20 @@ describe("measurement.Unit.Pressure", function() {
 			expect(pc[i].length).toBeGreaterThan(0);
 		}
 	});
-	
+
 	describe("convert():", function() {
+
 		var pressureConverter = measurement('Pressure');
-		it('1 Pressure.BAR equals 1 000 000 Pressure.PASCAL', function(){
+		it('1 Pressure.BAR equals 1 000 000 Pressure.PASCAL', function() {
 			var result = pressureConverter.convert(1).from(pc.BAR).to(pc.PASCAL);
-			expect( result ).toBeDefined();
-			expect( result ).toBe(1000000);
+			expect(result).toBeDefined();
+			expect(result).toBe(1000000);
 		});
-		console.log(pressureConverter);
-		it('1 Pressure.BAR equals 10 000 Pressure.HECTO_PASCAL ', function(){
-			console.log('yo');
+
+		it('1 Pressure.BAR equals 10 000 Pressure.HECTO_PASCAL ', function() {
 			var result = pressureConverter.convert(1).from(pc.BAR).to(pc.HECTOPASCAL);
-			expect( result ).toBeDefined();
-			expect( result ).toBe(10000);
-		});		
+			expect(result).toBeDefined();
+			expect(result).toBe(10000);
+		});
 	});
 });
